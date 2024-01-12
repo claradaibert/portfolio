818cb563ef6d0eb55434a4ca0c5a0f898c17f1c0
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-01-12 19:17:03 -0300

    Add new text

--- src/hooks/useTranslation/index.tsx
+++ src/hooks/useTranslation/index.tsx
@@ -11,13 +11,15 @@ i18n.use(initReactI18next).init({
     en: {
         translation: {
             '@home/title': "Hello.\nI'm Clara Daibert",
-            '@home/subtitle': "I'm a front-end developer based in Rio de Janeiro, Brazil"
+            '@home/subtitle': "I'm a front-end developer based in Rio de Janeiro, Brazil",
+            '@aboutMe/textBody': "I’m Clara, a  front-end developer with over 2 years of experience, creating and improving digital products for consumers and businesses. I’m always striving to learn more and love a good challenge.",
         }
     },
     pt: {
         translation: {
             '@home/title': "Oi.\nEu sou a Clara Daibert",
-            '@home/subtitle': "Eu sou uma desenvolvedora front-end do Rio de Janeiro"
+            '@home/subtitle': "Eu sou uma desenvolvedora front-end do Rio de Janeiro",
+            '@aboutMe/textBody': "Eu sou a Clara, uma desenvolvedora front-end com mais de 2 anos de experiência, criando e aprimorando produtos digitais para consumidores e empresas. Estou sempre buscando aprender cada vez mais e adoro um bom desafio",
         }
     }
   },
