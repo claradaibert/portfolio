f58839d2f5680c1dec9ea6372f0d2f3d670f10ad
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-01-16 19:20:18 -0300

    edit scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,11 +7,25 @@ const GlobalStyle = createGlobalStyle`
         outline: 0;
         box-sizing: border-box;
         max-width: 100vw;
+
+        ::-webkit-scrollbar {
+            width: 0.4rem;
+        }
+
+        ::-webkit-scrollbar-track {
+            background-color: ${({theme}) => theme.background_primary};
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: ${({theme}) => theme.background_secondary};
+            border-radius: 10px;
+        }
     }
 
     button {
         cursor: pointer;
     }
+
 `;
 
 export { GlobalStyle };
